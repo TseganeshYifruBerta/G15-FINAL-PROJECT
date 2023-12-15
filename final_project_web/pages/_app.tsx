91d01fdf528e36b165fb0c6635547c3136ed0d88
Tseganesh Yifru <tseganeshyifruberta>
2023-12-15 16:26:36 +0300

    implement sign-in sign-up

--- final_project_web/pages/_app.tsx
+++ final_project_web/pages/_app.tsx
@@ -1,17 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from '../reducer/reducers';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import {store} from "@/store";
 
-const store = configureStore({
-  reducer: rootReducer,
-});
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Component {...pageProps} />{" "}
     </Provider>
   );
 }
