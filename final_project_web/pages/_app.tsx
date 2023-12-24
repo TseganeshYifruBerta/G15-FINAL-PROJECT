acff20a3a4f081db073a0129a11196c87d600fb1
rozakassa22 <kassaroza22@gmail.com>
2023-12-24 12:11:20 +0300

    initial commit

--- final_project_web/pages/_app.tsx
+++ final_project_web/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from '../reducer/reducers';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
