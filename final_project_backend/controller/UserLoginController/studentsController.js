102a041fcd56cb483a63817298c3762ceb157c1b
Fenet Shewarega <shewaregafenet@gmail.com>
2023-12-24 12:47:42 -0500

    update controller

--- final_project_backend/controller/UserLoginController/studentsController.js
+++ final_project_backend/controller/UserLoginController/studentsController.js
@@ -1,5 +1,10 @@
 const Student = require("../../../models/registration/user/studentsModel.js");
 const jwt = require("jsonwebtoken");
+const crypto = require('crypto')
+const generateSecretKey = () => {
+  const secretLength = 32; // Adjust the length as per your requirements
+  return crypto.randomBytes(secretLength).toString('hex');
+};
 const studentLogin = async (req, res) => {
   const { userId, password } = req.body;
 
@@ -13,8 +18,8 @@ const studentLogin = async (req, res) => {
     if (student.password !== password) {
       return res.status(401).json({ message: "Invalid password" });
     }
-
-    const token = jwt.sign({ userId }, "your-secret-key");
+    const secretKey = generateSecretKey();
+    const token = jwt.sign({ userId }, secretKey);
 
     res.status(200).json({ token });
   } catch (error) {
--- final_project_backend/controller/UserLoginController/teachersController.js
+++ final_project_backend/controller/UserLoginController/teachersController.js
@@ -1,5 +1,10 @@
 const Teacher = require("../../../models/registration/user/teachersModel.js");
 const jwt = require("jsonwebtoken")
+const crypto = require('crypto')
+const generateSecretKey = () => {
+  const secretLength = 32; // Adjust the length as per your requirements
+  return crypto.randomBytes(secretLength).toString('hex');
+};
 const teacherLogin = async (req, res) => {
   const { email, password } = req.body;
 
@@ -12,7 +17,8 @@ const teacherLogin = async (req, res) => {
     if (teacher.password !== password) {
       return res.status(401).json({ message: "Invalid password" });
     }
-    const token = jwt.sign({email}, "your-secret-key")
+    const secretKey = generateSecretKey();
+    const token = jwt.sign({email}, secretKey);
     res.status(200).json({token});
   } catch (error) {
     res.status(500).json({ message: "An error occurred while logging in" });
