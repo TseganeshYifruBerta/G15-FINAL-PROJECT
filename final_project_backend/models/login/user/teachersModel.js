365266bb24b9b25604d1428facaf35d76de0cdb0
Fenet Shewarega <shewaregafenet@gmail.com>
2023-12-24 12:51:44 -0500

    update model

--- final_project_backend/models/login/user/teachersModel.js
+++ final_project_backend/models/login/user/teachersModel.js
@@ -1,22 +1,20 @@
-const Teacher = require("../../../models/registration/user/teachersModel.js");
-const jwt = require("jsonwebtoken")
-const teacherLogin = async (req, res) => {
-  const { email, password } = req.body;
+const { DataTypes } = require("sequelize");
+const sequelize = require("../../../database/sequelize");
+const Teacher = require("./teacher.model");
 
-  try {
-    const teacher = await Teacher.findOne({ where: { email } });
+const TeacherLogin = sequelize.define("TeacherLogin", {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
-    if (!teacher) {
-      return res.status(404).json({ message: "Teacher not found" });
-    }
-    if (teacher.password !== password) {
-      return res.status(401).json({ message: "Invalid password" });
-    }
-    const token = jwt.sign({email}, "your-secret-key")
-    res.status(200).json({token});
-  } catch (error) {
-    res.status(500).json({ message: "An error occurred while logging in" });
-  }
-};
+// Define associations between TeacherLogin and Teacher models
+TeacherLogin.belongsTo(Teacher, { foreignKey: "teacherId", targetKey: "id" });
 
-module.exports = { teacherLogin };
+module.exports = TeacherLogin;
