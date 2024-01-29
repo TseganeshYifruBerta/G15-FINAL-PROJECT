9ab751201c5f3eee97c98ab3e2552c1432af1b79
melatmesele <21melmes@gmail.com>
2024-01-29 13:52:59 +0300

    fixed

--- final_project_backend/routes/exam/examRoute.js
+++ final_project_backend/routes/exam/examRoute.js
@@ -5,7 +5,7 @@ const getAllExamQuestions = require("../../controller/exam/getAllExamQuestion")
 const getExamQuestionById = require("../../controller/exam/getEXamQuestionById")
 examRouters.post("/submitExam", submitExamQuestionWithTestCaseAndSolution);
 examRouters.get("/getAllExamQuestions" ,getAllExamQuestions);
-examRouters.get("/getExamQuestionById" ,getExamQuestionById)
+examRouters.get("/getExamQuestionById/:id" ,getExamQuestionById)
 
 
 module.exports = examRouters;
