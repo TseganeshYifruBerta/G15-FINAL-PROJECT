4dde138c0829522bd1185a21884774dd1197731b
FenetShewarega <shewaregafenet@gmail.com>
2024-01-29 02:10:29 -0500

    update and delete

---
+++ final_project_backend/controller/QuestionUploaderController/manageQuestion/deleteQuestion.js
@@ -0,0 +1,23 @@
+const { model } = require("../../../database/sequelize");
+const Question = require("../../../models/question_testcase_submission/question")
+const deleteQuestion = async (req, res) => {
+    const { questionId } = req.params;
+  
+    try {
+      const question = await Question.findByPk(questionId);
+      if (!question) {
+        return res.status(404).json({ error: "Question not found" });
+      }
+  
+      // Delete the question
+      await question.destroy();
+  
+      return res.status(200).json({ message: "Question deleted successfully" });
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  };
+  module.exports={
+
+    deleteQuestion,
+  }
