c7a36c1ee4889858b5f4be375e30410e9c286729
melatmesele <21melmes@gmail.com>
2024-02-18 18:54:19 +0300

    docker setup

--- final_project_backend/database/sequelize.js
+++ final_project_backend/database/sequelize.js
@@ -1,9 +1,9 @@
-   const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-   const sequelize = new Sequelize("project", "root", "", {
-     host: "localhost",
-     dialect: "mysql",
-     port: 3306, 
-   });
+const sequelize = new Sequelize('database', 'root', 'g15', {
+    host: 'localhost', 
+    dialect: 'mysql',
+    port: 3307, 
+});
 
-   module.exports = sequelize;
+module.exports = sequelize;
