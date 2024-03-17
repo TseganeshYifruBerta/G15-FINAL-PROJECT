5b8e60b6297cd19e4eb994faf727d6d6b8641901
melatmesele <21melmes@gmail.com>
2024-03-17 16:26:32 +0300

    submitted Student Exam Answer

---
+++ final_project_backend/controller/exam/submittedExamAnswer/submittedStudentsExamAnswer.js
@@ -0,0 +1,49 @@
+const sequelize  = require("../../../database/sequelize");
+const studentsExamAnswer = require("../../../models/exam/studentsExamAnswer");
+const studentsExamDetail = require("../../../models/exam/submittedExamDetail");
+
+
+const submitExamAnswerByStudent = async (req, res) => {
+  const { examId, userId, questionAndSolution } = req.body;
+
+  const t = await sequelize.transaction();
+  
+  try {
+    const studentsExamAnswers = await studentsExamAnswer.create(
+      {
+        examId,
+        userId,
+      },
+      { transaction: t }
+    );
+
+    const questionAndSolutions = await Promise.all(
+      questionAndSolution.map(async (questionAndSolution) => {
+        return await studentsExamDetail.create(
+          {
+            questionId: questionAndSolution.questionId,
+            submittedAnswer: questionAndSolution.submittedAnswer,
+            studentsExamAnswerId: studentsExamAnswers.id,
+          },
+          { transaction: t }
+        );
+      })
+    );
+
+    await t.commit();
+
+    res.status(201).json({
+      message: "Question and test cases submitted successfully",
+      ExamAndUser: studentsExamAnswers,
+      QuestionAndAnswer: questionAndSolutions,
+    });
+  } catch (error) {
+    await t.rollback();
+    res.status(500).json({
+      message: "Error submitting question and test cases",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = submitExamAnswerByStudent;
