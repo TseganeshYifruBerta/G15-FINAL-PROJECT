ca20c03c2b03d32d315346a37ce8d7c8910fa492
FenetShewarega <shewaregafenet@gmail.com>
2024-03-17 14:34:54 +0300

    updates

--- final_project_backend/controller/UserDataUploaderController/numberOfStudent.js
+++ final_project_backend/controller/UserDataUploaderController/numberOfStudent.js
@@ -1,4 +1,5 @@
 const Student = require("../../models/registration/user/studentsModel");
+const Teacher = require("../../models/registration/user/teachersModel");
 const Question = require("../../models/registration/user/studentsModel"); // Import the LabQuestion and TestCase models
 
 const getNumberOfAllStudent = async(req,res) =>{
@@ -12,4 +13,15 @@ const getNumberOfAllStudent = async(req,res) =>{
      return res.sendStatus(400);
     }
 };
-module.exports =  getNumberOfAllStudent
+const getNumberOfAllTeacher = async(req,res) =>{
+    try{
+      const teacher = await Teacher.findAll();
+      const count = teacher.length;
+      return res.status(200).json(count)
+     
+    } catch(error){
+     console.log(error);
+     return res.sendStatus(400);
+    }
+}
+module.exports =  {getNumberOfAllStudent , getNumberOfAllTeacher}
--- final_project_backend/controller/codeExecution/countAcceptedsubmission.js
+++ final_project_backend/controller/codeExecution/countAcceptedsubmission.js
@@ -24,8 +24,32 @@ const countAcceptedSubmissions = async (req, res) => {
       console.log(error);
       return res.status(500).json({ error: "Internal Server Error" });
     }
-  };
+}
   
-  module.exports = 
-    countAcceptedSubmissions
   
+const getAllAcceptedSubmissions= async (req, res) => {  
+  
+  try {
+    const submissions = await codeSubmision.findAll();
+    let allAcceptedSubmissions = [];
+    for (const submission of submissions) {
+      const status = await Status.findOne({
+
+
+        where: {  submittedCodeId: submission.id, status: 'Accepted' }
+
+      });
+      if (status) {
+        allAcceptedSubmissions.push(submission);
+      }}
+    return res.status(200).json(allAcceptedSubmissions);
+  }
+  catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+
+
+}
+
+module.exports = { countAcceptedSubmissions, getAllAcceptedSubmissions }; 
