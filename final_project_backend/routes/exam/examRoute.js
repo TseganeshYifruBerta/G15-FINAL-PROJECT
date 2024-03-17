165ce02f64b528a0e21fdbd5bf3180bc17e4b620
melatmesele <21melmes@gmail.com>
2024-03-17 14:03:25 +0300

    created exam detail

--- final_project_backend/routes/exam/examRoute.js
+++ final_project_backend/routes/exam/examRoute.js
@@ -6,18 +6,18 @@ const getExamQuestionById = require("../../controller/exam/examQuestion/getEXamQ
 const {createExam,updateCreatedExam,deleteCreatedExam,startCreatedExam }=require("../../controller/exam/createExam/examController")
 const deleteExamQuestion = require("../../controller/exam/examQuestion/manageExamQuestion/deleteExamQuestion")
 const editExamQuestion = require("../../controller/exam/examQuestion/manageExamQuestion/updateExamQuestion")
-const {getAllExams , getExamById} = require("../../controller/exam/createExam/getExam")
+const {getAllCreatedExams , getCreatedExamById} = require("../../controller/exam/createExam/getExam")
 
 examRouters.post("/uploadExamQuestion", submitExamQuestionWithTestCaseAndSolution);
 examRouters.get("/getAllExamQuestions" ,getAllExamQuestions);
 examRouters.get("/getExamQuestionById/:id" ,getExamQuestionById)
-examRouters.delete("/deleteExamQuestionById/:examQuesId" ,deleteExamQuestion);
-examRouters.put("/updateExamQuestionById/:examQuesId" ,editExamQuestion);
+examRouters.delete("/deleteExamQuestionById/:id" ,deleteExamQuestion);
+examRouters.put("/updateExamQuestionById/:id" ,editExamQuestion);
 examRouters.post("/createExam", createExam);
 examRouters.put("/updateExam/:id", updateCreatedExam);
 examRouters.delete("/deleteExam/:id", deleteCreatedExam);
 examRouters.put("/startExam/:id", startCreatedExam);
-examRouters.get("/getAllExams", getAllExams);
-examRouters.get("/getExamById/:id", getExamById);
+examRouters.get("/getAllExams", getAllCreatedExams);
+examRouters.get("/getCreateExamById/:id", getCreatedExamById);
 
 module.exports = examRouters;
