5f41ed07e9a672cdcbaf1f3bd0b109fcd36975ea
melatmesele <21melmes@gmail.com>
2024-03-20 01:46:22 +0300

    fix someissue and adding routes

--- final_project_backend/routes/userDataUploader/userData.js
+++ final_project_backend/routes/userDataUploader/userData.js
@@ -5,13 +5,27 @@ const  uploadUsersFile  = require("../../controller/userDataUploadController/dat
 const submitUserfile = require("../../controller/userDataUploadController/inputDataUploader")
 const verifyRoles = require("../../middleware/verifyRoles")
 const userUpload = multer({ dest: "uploads/user" });
-const {isAdmin} = require("../../middleware/roleMiddleWare");
+const {isAdmin, isTeacher} = require("../../middleware/roleMiddleWare");
+const {fetchAllStudentBasedOnSection,findStudentByUserId ,updateUser} = require("../../controller/userDataUploadController/userUploadManagment");
 
-
-
+            //  upload user data
 dataUploadRouters.post("/userDataUploader", userUpload.single("usersExcelFile"),verifyRoles("admin"),isAdmin, uploadUsersFile);
-
+ 
+            //  input user data
 dataUploadRouters.post("/inputUserDataUploader", verifyRoles("admin"),isAdmin, submitUserfile);
+ 
+            //  fetch all uploaded student data based on teacher's section
+dataUploadRouters.get("/fetchAllStudentBasedOnSection/:id", verifyRoles("teacher"),isTeacher, fetchAllStudentBasedOnSection);
+
+            // fetch all uploaded student data based on userID 
+            // questioned
+            
+dataUploadRouters.get("/findStudentByUserId/:id",  verifyRoles("teacher"),isTeacher,findStudentByUserId );
+
+            // update user data, only admin can update
+dataUploadRouters.put("/updateUser/:id", verifyRoles("admin"),isAdmin,updateUser );
+
+
 
 
 module.exports = dataUploadRouters;
