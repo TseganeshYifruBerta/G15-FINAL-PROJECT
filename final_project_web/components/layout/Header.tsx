690b88c9a89a4a1fb7bbe8bf0fb377b004f15422
Tseganesh Yifru <tseganeshyifruberta>
2024-03-20 13:07:14 +0300

    remove signup, role based

--- final_project_web/components/layout/Header.tsx
+++ final_project_web/components/layout/Header.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
 import { Link as LinkScroll } from "react-scroll";
-import ButtonOutline from "@/components/misc/ButtonOutline"; // Assuming ButtonOutline is already a TypeScript componen
 import LogoVPN from "@/public/assets/Logo.svg"; // Make sure SVG loading is correctly set up in your TS config
+import SignInSide from "@/pages/signin";
 
 type ActiveLink = "about" | "feature" | "pricing" | "testimoni" | null;
 
@@ -22,12 +20,18 @@ const Header: React.FC = () => {
 
   return (
     <>
-      <header className={"fixed top-0 w-full z-30 bg-white transition-all " + (scrollActive ? "shadow-md pt-0" : "pt-4")}>
+      <header
+        className={
+          "fixed top-0 w-full z-30 bg-white transition-all " +
+          (scrollActive ? "shadow-md pt-0" : "pt-4")
+        }
+      >
         <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-6 mx-auto grid grid-flow-col py-3 sm:py-4">
           <div className="col-start-1 col-end-2 flex items-center">
-          
-          <LogoVPN className="h-8 w-auto " />
-          <p className="text-lg font-bold">ሁሉ <span className="text-[#7983FB]">Code</span> </p>
+            <LogoVPN className="h-8 w-auto " />
+            <p className="text-lg font-bold">
+              ሁሉ <span className="text-[#7983FB]">Code</span>{" "}
+            </p>
           </div>
           <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
             <LinkScroll
@@ -45,7 +49,6 @@ const Header: React.FC = () => {
                   ? "text-[#7983FB] animation-active border-b-2 border-[#7983FB] "
                   : "text-black-500 hover:text-[#7983FB] hover:border-b-2 hover:border-[#7983FB] ")
               }
-              
             >
               Home
             </LinkScroll>
@@ -64,7 +67,6 @@ const Header: React.FC = () => {
                   ? "text-[#7983FB] animation-active border-b-2 border-[#7983FB] "
                   : "text-black-500 hover:text-[#7983FB] hover:border-b-2 hover:border-[#7983FB] ")
               }
-              
             >
               Functions
             </LinkScroll>
@@ -83,7 +85,6 @@ const Header: React.FC = () => {
                   ? "text-[#7983FB] animation-active border-b-2 border-[#7983FB] "
                   : "text-black-500 hover:text-[#7983FB] hover:border-b-2 hover:border-[#7983FB] ")
               }
-              
             >
               Description
             </LinkScroll>
@@ -96,24 +97,30 @@ const Header: React.FC = () => {
               onSetActive={() => {
                 setActiveLink("testimoni");
               }}
-              className={
-                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative " +
-                (activeLink === "testmoni"
-                  ? "text-[#7983FB] animation-active border-b-2 border-[#7983FB] "
-                  : "text-black-500 hover:text-[#7983FB] hover:border-b-2 hover:border-[#7983FB] ")
-              }
-              
+              // className={
+              //   "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative " +
+              //   (activeLink === "testmoni"
+              //     ? "text-[#7983FB] animation-active border-b-2 border-[#7983FB] "
+              //     : "text-black-500 hover:text-[#7983FB] hover:border-b-2 hover:border-[#7983FB] ")
+              // }
+              className={`px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative ${
+                (activeLink as string) === "testmoni"
+                  ? "text-[#7983FB] animation-active border-b-2 border-[#7983FB]"
+                  : "text-black hover:text-[#7983FB] hover:border-b-2 hover:border-[#7983FB]"
+              }`}
             >
               Features
             </LinkScroll>
           </ul>
           <div className="col-start-10 col-end-12 font-medium flex justify-end items-center">
-            <Link href="/" className="text-black-600 mx-2 sm:mx-4 capitalize tracking-wide hover:text-[#7983FB] transition-all">
-            
-                  Sign In
-          
+            {/* <Link
+              href="/"
+              className="text-black-600 mx-2 sm:mx-4 capitalize tracking-wide hover:text-[#7983FB] transition-all"
+            >
+                Sign In
             </Link>
-            <ButtonOutline>Sign Up</ButtonOutline>
+            <ButtonOutline>Sign Up</ButtonOutline> */}
+            <SignInSide />
           </div>
         </nav>
       </header>
