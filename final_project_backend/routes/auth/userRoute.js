0837cddb641dc85ad0d5959e24bed7193524cd65
melatmesele <21melmes@gmail.com>
2024-03-27 19:09:05 +0300

    fetch all student

--- final_project_backend/routes/auth/userRoute.js
+++ final_project_backend/routes/auth/userRoute.js
@@ -2,11 +2,13 @@
 const express = require("express");
 const router = express.Router();
 const login = require("../../controller/userAuthorization/userLogin");
-
+const {fetchAllStudentBasedOnSection} = require("../../controller/userDataUploadController/userUploadManagment");
 
 
 
 
 router.post("/userLogin", login);
+
+router.get("/fetchAllStudentBasedOnSection/:teacherId",fetchAllStudentBasedOnSection );
 
 module.exports = router;
