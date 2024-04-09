cb87ad58c3ee315862d5b8b891d7cf1ef2296c13
ekram <ekruiwc2020@gmail.com>
2024-04-09 11:36:45 +0300

    feat: submission detail implementation

--- final_project_web/pages/submissions/[id]/index.tsx
+++ final_project_web/pages/submissions/[id]/index.tsx
@@ -1,70 +1,124 @@
 import Navigation from "@/components/layout/Navigation";
 import NavigationTeacher from "@/components/layout/NavigationTeacher";
-import { useGetSubmissionDetailQuery } from "@/store/submissions/get-submission-detail-by-id-api"
+import { useGetSubmissionDetailQuery } from "@/store/submissions/get-submission-detail-by-id-api";
 import { Editor } from "@monaco-editor/react";
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import { useState } from "react";
+
+
 interface SubmissionDetailProps {
-  id: number,
-  status: string,
-  userId: string
-  questionId: string,
-  userCode:string,
-  createdAt: string,
-  updatedAt: string;
-  submittedCodeId: number;
+  questionStatus: {
+    id: number;
+    status: string;
+    questionId: string;
+    userCode: string;
+    createdAt: string;
+    updatedAt: string;
+    UserinformationId: number;
+    submittedCodeId: number;
+  };
+  questionSubmitted: {
+    id: number;
+    title: string;
+    difficulty: string;
+    description: string;
+    example: string;
+    teacherId: string;
+    functionName: string;
+    createdAt: string;
+    updatedAt: string;
+  };
 }
-function SubmissionDetailById () {
-    const router = useRouter()
-  const submitId = router.query.id as string;
 
 
-    const { data:submissionDetail, isLoading, isError } = useGetSubmissionDetailQuery({ submitId: submitId });
-    if(isLoading){
-        return <div>Loading</div>
-    }
-    console.log(submissionDetail)
-    const {
-  id,
-  status,
-  userId,
-  questionId,
-  userCode,
-  createdAt,
-  updatedAt,
-  submittedCodeId
-} = submissionDetail
+function SubmissionDetailById() {
+  const router = useRouter();
+  const { id: queryId } = router.query; 
+
+  if (!queryId) {
+    return <div>No submission ID provided</div>;
+  }
+
+  const submitId = queryId as string;
+  const { data: submissionDetail, isLoading, isError } = useGetSubmissionDetailQuery({ submitId });
+  const currentDate = new Date();
+
+  const updatedAtTimestamp = new Date(submissionDetail.questionSubmitted.updatedAt);
+  const timeDifference = currentDate.getTime() - updatedAtTimestamp.getTime();
+  const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+  let timeAgo: string;
+
+  if (daysAgo < 7) {
+      timeAgo = `${daysAgo} days ago`;
+  } else if (daysAgo <= 30) {
+      const weeksAgo = Math.floor(daysAgo / 7);
+      timeAgo = `${weeksAgo} weeks ago`;
+  } else if (daysAgo < 365) {
+      const monthsAgo = Math.floor(daysAgo / 30);
+      timeAgo = `${monthsAgo} months ago`;
+  } else {
+      const yearsAgo = Math.floor(daysAgo / 365);
+      timeAgo = `${yearsAgo} years ago`;
+  }
 
 
-    return (
-      <div className="flex">
-        <div className="w-2/6">
-          <Navigation />
-        </div>
-        <div className="w-4/6">
-          <div className="  min-h-screen p-4">
-            <div className="text-2xl font-bold flex justify-center mb-2">
-              <span>Submission {id} Details</span>
-            </div>
-            <div className="">
-              <div className="w-1/5">Created At</div>
-              <div className="w-3/5 text-primary">{createdAt}</div>
-            </div>
-            <div className="">
-              <div className="mb-2">Submitted Code</div>
-              <div className="w-4/5">
-                <Editor
-                  height="70vh"
+  const linesCount = submissionDetail.questionStatus.userCode.split('\n').length;
+  const height = Math.max(30 * linesCount); 
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError || !submissionDetail) {
+    return <div>Error loading submission detail</div>;
+  }
+
+  return (
+    <div className='p-8'>
+        <h3 className='text-xl text-primary'>
+          {submissionDetail.questionSubmitted.title}
+        </h3>
+        <div className='flex flex-col mt-3 gap-3'>
+          <div className='text-3xl'>
+            Submission Detail
+          </div>
+          <div className='border-4 border-double border-primary color-primary p-3 text-primary'>
+              <div className="flex flex-row">
+                <div className="mr-2 text-gray-500 ">Status: </div>
+                <div className="text-red-500"> {submissionDetail.questionStatus.status}</div>
+              </div>
+              <div className="flex flex-row">
+                <div className="mr-2 text-gray-500">Submitted: </div>
+                <div className="text-primary">{timeAgo}</div>
+              </div>
+
+          </div>
+          <div className='flex flex-row space-x-[180px] md:space-x-[960px]'>
+              <div className="flex flex-row">
+                <div className="mr-2 text-gray-500">Language: </div>
+                <div className="text-black">Python</div>
+              </div>
+              <button type="button" className="focus:outline-none text-white bg-primary hover:bg-primary focus:ring-4 focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary">Edit</button>
+          </div>
+          <div>
+              <Editor
+                  height={height}
                   language={"python"}
                   theme={"vs-dark"}
-                  value={userCode}
-                />
-              </div>
-            </div>
+                  value={submissionDetail.questionStatus.userCode}
+
+              />
           </div>
+          <a href="#" className="inline-flex items-center justify-center font-medium text-primary hover:underline mt-3">
+           Back to code 
+          <svg className="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+          </svg>
+          </a> 
         </div>
       </div>
-    );
+  )
 }
 
-export default SubmissionDetailById
+export default SubmissionDetailById;
