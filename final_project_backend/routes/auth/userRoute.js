3d660f89ccdd8ea4fed330416650250a5fad5daf
FenetShewarega <shewaregafenet@gmail.com>
2024-04-10 11:51:49 +0300

    route edit

--- final_project_backend/routes/auth/userRoute.js
+++ final_project_backend/routes/auth/userRoute.js
@@ -2,10 +2,6 @@
 const express = require("express");
 const router = express.Router();
 const login   = require("../../controller/userAuthorization/userLogin");
-// const activateUser = require("../../controller/userAuthorization/userLogin");
-const { isAdmin } = require("../../middleware/roleMiddleWare");
-const verifyRoles = require("../../middleware/verifyRoles");
-
 
 router.post("/userLogin", login);
 
