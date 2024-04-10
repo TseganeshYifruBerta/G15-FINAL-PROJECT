cd6e2964b29171744d888a8de16f5ebca0b0db71
FenetShewarega <shewaregafenet@gmail.com>
2024-04-10 09:25:26 +0300

    admin

--- final_project_backend/controller/userAuthorization/userLogin.js
+++ final_project_backend/controller/userAuthorization/userLogin.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../../models/auth/user.model');
 const Section = require('../../models/auth/section.model');
+const e = require('express');
 const login = async (req, res) => {
     const { userId, password } = req.body;
 
@@ -9,6 +10,28 @@ const login = async (req, res) => {
 
     if (!user || !bcrypt.compareSync(password, user.password)) {
         return res.status(401).json({ message: 'Invalid userId or password' });
+    }
+    if (user.status === 'inactive') {
+        return res.status(401).json({ message: 'User is inactive' });
+    }
+
+    if (bcrypt.compareSync(password, user.password)) {
+     user.loginAttempt = (user.loginAttempt || 0)+ 1;
+
+        if (user.loginAttempt >= 5) {
+            user.status = 'inactive';
+            user.save();
+            return res.status(401).json({ message: 'User is inactive' });
+        }
+        else {
+            
+            await user.save();
+            return res.status(401).json({ message: 'User is inactive' });
+        }
+
+
+
+          
     }
     const section = await Section.findAll({ where: { UserinformationId: user.id } });
     const sectionValues = section.map(sec => sec.section);
@@ -27,6 +50,23 @@ const login = async (req, res) => {
     console.log("decodedToken",decodedToken)
   
     res.json({ token });
-};
 
-module.exports =  login ;
+}
+
+
+    const activateUser = async (req, res) => {
+        const { userId } = req.body;
+        const user  = await User.findOne({ where: { userId }
+         });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid userId' });
+
+        }
+        user.status = 'active';
+        await user.save();
+        res.json({ message: 'User is activated' });
+    }
+
+
+
+module.exports = { login, activateUser };          
