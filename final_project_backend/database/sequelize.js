5d2f7d3640d47b53ed4cd086835406ef0763f75b
melatmesele <21melmes@gmail.com>
2024-05-06 14:01:19 +0300

    deployment step 1

--- final_project_backend/database/sequelize.js
+++ final_project_backend/database/sequelize.js
@@ -1,9 +1,10 @@
    const { Sequelize } = require('sequelize');
 
-   const sequelize = new Sequelize("database", "root", "", {
-     host: "localhost",
+   const sequelize = new Sequelize("sql11704387", "sql11704387", "Cp4WwKKu4W", {
+     host: "sql11.freemysqlhosting.net",
      dialect: "mysql",
      port: 3306,
    });
 
 module.exports = sequelize;
+
