37156a77968c9546063c013048b3959f4ada503f
ekram <ekruiwc2020@gmail.com>
2024-05-14 18:27:44 +0300

    feat: count accepted submissions for each difficulty

---
+++ final_project_web/store/submissions/count-Accepted-submissions-per-Difficulty.tsx
@@ -0,0 +1,41 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const jwt = require("jsonwebtoken");
+
+const baseUrl = ""; 
+
+export interface difficultyData{
+    easyCount: number,
+    mediumCount: number,
+    hardCount: number,
+}
+
+
+export const countAcceptedSubmissionsperDifficultyApi = createApi({
+  reducerPath: "countAcceptedSubmissionsperDifficultyApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: baseUrl,
+    prepareHeaders: (headers, { getState }) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    countAcceptedSubmissionsperDifficultyApi: builder.query<difficultyData[], void>({
+      query: () => {
+        const token = localStorage.getItem("token");
+        const decodedToken: { id: number } = jwt.decode(token) as { id: number };
+        const userId = decodedToken.id;
+        let url = "http://localhost:5000/codeSubmission/countAcceptedSubmissionsPerUser";
+        return {
+          url: `${url}/${userId}`,
+          method: "GET",
+        };
+      },
+    }),
+  }),
+});
+
+export const { useCountAcceptedSubmissionsperDifficultyApiQuery } = countAcceptedSubmissionsperDifficultyApi;
