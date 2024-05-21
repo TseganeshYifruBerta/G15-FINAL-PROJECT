b65d28a2bc099f54c174ee81f73f09cb8d04c2f4
melatmesele <21melmes@gmail.com>
2024-05-21 22:51:22 +0300

    deployment

--- final_project_backend/database/sequelize.js
+++ final_project_backend/database/sequelize.js
@@ -1,10 +1,10 @@
-   const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-   const sequelize = new Sequelize("sql11704387", "sql11704387", "Cp4WwKKu4W", {
-     host: "sql11.freemysqlhosting.net",
-     dialect: "mysql",
-     port: 3306,
-   });
+const sequelize = new Sequelize("sql8708187", "sql8708187", "U1der92rig", {
+  host: "sql8.freemysqlhosting.net",
+  dialect: "mysql",
+  port: 3306,
+});
 
 module.exports = sequelize;
 
