77bf8550ac8e7a2591b7815cc60f4bff47a9797f
Tseganesh Yifru <tseganeshyifruberta>
2024-05-24 09:25:36 +0300

    backend sql

--- final_project_backend/database/sequelize.js
+++ final_project_backend/database/sequelize.js
@@ -1,10 +1,9 @@
-   const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-   const sequelize = new Sequelize("final", "root", "", {
-     host: "localhost",
-     dialect: "mysql",
-     port: 3306,
-   });
+const sequelize = new Sequelize("sql8708187", "sql8708187", "U1der92rig", {
+  host: "sql8.freemysqlhosting.net",
+  dialect: "mysql",
+  port: 3306,
+});
 
 module.exports = sequelize;
-
