5d3ed3425301eaf31e968702ba2c1632fe8c1eaa
melatmesele <21melmes@gmail.com>
2024-05-27 11:40:41 +0300

    adding logger

---
+++ final_project_backend/logger.js
@@ -0,0 +1,18 @@
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} ${level}: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'error.log', level: 'error' })
+  ]
+});
+
+module.exports = logger;
