df465709e10b6d54934325fa72f1ed5673bf824d
melatmesele <21melmes@gmail.com>
2024-05-28 11:02:51 +0300

    chg

--- final_project_backend/controller/codeExecution/codeExecution.js
+++ final_project_backend/controller/codeExecution/codeExecution.js
@@ -90,7 +90,7 @@ const runPythonCode = (pythonCode, nums) => {
               const pythonProcess = spawn(pythonExecutable, fileContents.split(/\r?\n/), { shell: true });
               // logger.info(`Python pooooooooooo: ${tempFilePath}`);
 
-              logger.info(`Python process started with executable: ${pythonProcess.spawnargs[1]}`);
+              logger.info(`Python process started with executable: ${pythonProcess}`);
               let result = "";
               let printOutput = "";
 
