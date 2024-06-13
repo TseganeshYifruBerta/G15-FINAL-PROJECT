77537ddb139a7058acef975bacb636da1d94541c
ekram <ekruiwc2020@gmail.com>
2024-06-13 14:44:27 +0300

    refactor: update code submissions

--- final_project_web/pages/student/submissions/index.tsx
+++ final_project_web/pages/student/submissions/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Link from "next/link";
-import { Submission, useGetAllSubmissionsByIdQuery } from "@/store/submissions/get-all-submissions-by-id";
+import { Submission, useGetAllSubmissionsByIdQuery } from "./../../../store/submissions/get-all-submissions-by-id";
+import Loading from "./../../../components/common/Loading";
+import Image from 'next/image';
 
 const SubmissionsPage = () => {
   const {
@@ -8,97 +10,114 @@ const SubmissionsPage = () => {
     error,
     isLoading,
   } = useGetAllSubmissionsByIdQuery();
-
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <Loading />;
   if (error) return <div>Error</div>;
 
   return (
-    <div className="mx-24 relative overflow-x-auto border border-gray-200 shadow-md sm:rounded-lg">
-      <table className="w-full align-center justify-center text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-l text-primary uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3 border-r border-gray-300">
-              Title
-            </th>
-            <th scope="col" className="px-6 py-3 border-r border-gray-300">
-              Difficulty
-            </th>
-            <th scope="col" className="px-6 py-3 border-r border-gray-300">
-              Status
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Time Submitted
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {submissions?.map((submission: Submission) => (
-            <tr
-              key={submission.id}
-              className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-100 even:dark:bg-gray-800 border-b dark:border-gray-700 cursor-pointer"
-            >
-              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300">
-                <Link href={`/submissions/${submission.id}`}>
-                  <p>{submission.questionsForId.title}</p>
-                </Link>
-              </td>
-              <td className="px-6 py-4 border-r border-gray-300">
-                {submission.questionsForId.difficulty === "easy" && (
-                  <Link href={`/submissions/${submission.id}`}>
-                    <span style={{ color: "green" }}>Easy</span>
-                  </Link>
-                )}
-                {submission.questionsForId.difficulty === "medium" && (
-                  <Link href={`/submissions/${submission.id}`}>
-                    <span style={{ color: "orange" }}>Medium</span>
-                  </Link>
-                )}
-                {submission.questionsForId.difficulty === "hard" && (
-                  <Link href={`/submissions/${submission.id}`}>
-                    <span style={{ color: "red" }}>Hard</span>
-                  </Link>
-                )}
-              </td>
-              <td className="px-6 py-4 border-r border-gray-300">
-                {submission.questionStatus && (
-                  <>
-                    {submission.questionStatus.status === "Accepted" && (
-                      <Link href={`/submissions/${submission.id}`}>
-                        <span style={{ color: "green" }}>Accepted</span>
-                      </Link>
-                    )}
-                    {submission.questionStatus.status === "Wrong" && (
-                      <Link href={`/submissions/${submission.id}`}>
-                        <span style={{ color: "red" }}>Wrong</span>
-                      </Link>
-                    )}
-                  </>
-                )}
-              </td>
-              <td className="px-6 py-4 border-r border-gray-300">
-                <div className="flex flex-col">
-                  {submission.questionStatus?.updatedAt && (
-                    <Link href={`/submissions/${submission.id}`}>
-                      <span>
-                        {new Date(
-                          submission.questionStatus.updatedAt
-                        ).toLocaleString([], {
-                          month: "2-digit",
-                          day: "2-digit",
-                          year: "numeric",
-                          hour: "2-digit",
-                          minute: "2-digit",
-                        })}
-                      </span>
-                    </Link>
-                  )}
-                </div>
-              </td>
+    <div>
+      {submissions?.length === 0 ? (
+          <div className="col-span-12 rounded-lg px-5 pb-5 pt-7.5 dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
+          <div className="flex flex-col items-center justify-center text-center">
+            <Image
+              src="/images/nodata.svg"
+              className="w-16 h-16 mb-4 text-gray-400 dark:text-gray-500"
+              alt=""
+              width={12}
+              height={12}
+            />
+            <h3 className="mb-2 text-l font-semibold text-gray-800 dark:text-gray-200">
+              No Submissions Yet
+            </h3>
+            <p className="text-md text-gray-600 dark:text-gray-400">
+              It looks like there are no submission at the moment!
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="mx-24 mt-12 relative overflow-x-auto bg-primary sm:rounded-lg">
+        <table className="w-full align-center justify-center text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-md text-gray-600 dark:text-white bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3 border-r border-gray-300">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3 border-r border-gray-300">
+                Difficulty
+              </th>
+              <th scope="col" className="px-6 py-3 border-r border-gray-300">
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Time Submitted
+              </th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {submissions?.map((submission: Submission) => (
+                <tr
+                    key={submission.id}
+                    className="bg-white rounded-lg dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 cursor-pointer"
+                >
+                    <td className="px-6 py-4 font-medium text-gray-500 dark:text-white whitespace-nowrap">
+                        <Link href={`/submissions/${submission.id}`}>
+                            <span className="text-sm">{submission.questionsForId.title}</span>
+                        </Link>
+                    </td>
+                    <td className="px-6 py-4 text-sm">
+                                <span
+                                    className={
+                                        submission.questionsForId.difficulty === 'Easy'
+                                            ? "text-green-500 bg-green-500 bg-opacity-20 px-4 py-1 rounded-xl ml-2"
+                                            : submission.questionsForId.difficulty === 'Medium'
+                                            ? "text-yellow-800 bg-yellow-800 bg-opacity-20 px-4 py-1 rounded-xl"
+                                            : submission.questionsForId.difficulty === 'Hard'
+                                            ? "text-red-500 bg-red-500 bg-opacity-20 px-4 py-1 rounded-xl ml-2"
+                                            : "text-yellow-800 bg-yellow-800 bg-opacity-20 px-4 py-1 rounded-xl"
+                                    }
+                                >
+                            {submission.questionsForId.difficulty.charAt(0).toUpperCase() + submission.questionsForId.difficulty.slice(1)}
+                        </span>
+                    </td>
+                    <td className="px-6 py-4">
+                        {submission.questionStatus?.status !== null ? (
+                            <Link href={`/submissions/${submission.id}`}>
+                                {submission.questionStatus?.status === 'Accepted' ? (
+                                    <span className="text-green-500 text-sm">Accepted</span>
+                                ) : submission.questionStatus?.status === 'Wrong Answer' ? (
+                                    <span className="text-red-500 text-sm">Wrong Answer</span>
+                                ) : (
+                                    <span className="text-red-500 text-sm">Error Occured</span>
+                                )}
+                            </Link>
+                        ) : (
+                            <span className="text-yellow-500 text-sm">Error</span>
+                        )}
+                    </td>
+                    <td className="px-6 py-4">
+                        <div className="flex flex-col">
+                            {submission.questionsForId?.updatedAt && (
+                                <Link href={`/submissions/${submission.id}`}>
+                                    <span className="text-sm font-medium text-gray-500">
+                                        {new Date(submission.questionsForId.updatedAt).toLocaleString([], {
+                                            month: '2-digit',
+                                            day: '2-digit',
+                                            year: 'numeric',
+                                            hour: '2-digit',
+                                            minute: '2-digit',
+                                        })}
+                                    </span>
+                                </Link>
+                            )}
+                        </div>
+                    </td>
+                </tr>
+            ))}
         </tbody>
-      </table>
-    </div>
+        </table>
+      </div>
+      )}
+  </div>
+    
   );
 };
 
