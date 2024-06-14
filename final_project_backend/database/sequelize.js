cac35dfc19a669d0c21a1dc5cbe85521f2008b1d
FenetShewarega <shewaregafenet@gmail.com>
2024-06-15 02:32:54 +0300

    update

--- final_project_backend/database/sequelize.js
+++ final_project_backend/database/sequelize.js
@@ -3,10 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("roz_profile", "root", "", {
   host: "localhost",
   dialect: "mysql",
-  port: 3308,
-  dialectOptions: {
-    connectTimeout: 60000  // 60 seconds
-  }
+  port: 3306,
 });
 
 module.exports = sequelize;
