96c74d1272f04d92da7cab8e7b3925343a22cade
melatmesele <21melmes@gmail.com>
2024-06-15 03:08:03 +0300

    increasing timeout

--- final_project_backend/database/sequelize.js
+++ final_project_backend/database/sequelize.js
@@ -4,6 +4,15 @@ const sequelize = new Sequelize("sql12712404", "sql12712404", "CWVcNymWhE", {
   host: "sql12.freemysqlhosting.net",
   dialect: "mysql",
   port: 3306,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 60000, // Increase the acquire timeout to 60 seconds
+    idle: 10000
+  },
+  dialectOptions: {
+    connectTimeout: 60000 // Increase the connection timeout to 60 seconds
+  }
 });
 
 module.exports = sequelize;
