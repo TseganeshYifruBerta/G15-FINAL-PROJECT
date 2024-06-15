920c61e4e7cbd24b3891295cf0e1d7494ef55230
rozakassa22 <kassaroza27@gmail.com>
2024-06-15 21:48:52 +0300

    update

--- final_project_web/components/changepassword/changepasswordpopup.tsx
+++ final_project_web/components/changepassword/changepasswordpopup.tsx
@@ -34,6 +34,7 @@ const PasswordChangeComponent: React.FC<PasswordChangeComponentProps> = ({ showF
       showToast('User ID is missing', 'error');
       return;
     }
+    
 
     try {
       await changePassword({ userId, newPassword, oldPassword });
@@ -77,8 +78,11 @@ console.log('old password', oldPassword)
                   onChange={(e) => setNewPassword(e.target.value)}
                   className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md mb-3 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                 />
+                {newPassword.length <= 6 && (
+    <p className="text-red-500 text-xs mt-1">Password must be longer than 6 characters.</p>
+  )}
               </div>
-              <button type="submit" className="bg-[#7983FB] bg-opacity-30 text-[#7983FB] hover:bg-[#7983FB] hover:bg-opacity-60 font-bold py-2 px-4 rounded">
+              <button type="submit" className="bg-[#7983FB] mt-3 bg-opacity-30 rounded-lg shadow-lg text-[#7983FB] hover:bg-[#7983FB] hover:bg-opacity-60 font-bold py-2 px-4 rounded">
                 Submit
               </button>
             </form>
--- final_project_web/components/components/Header/index.tsx
+++ final_project_web/components/components/Header/index.tsx
@@ -24,7 +24,7 @@ const Header = (props: {
         <div className="flex items-center gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             {/* <DarkModeSwitcher /> */}
-            <DropdownNotification />
+            {/* <DropdownNotification /> */}
           </ul>
           <DropdownUser />
         </div>
