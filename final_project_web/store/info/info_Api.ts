94ae73e9c4d1ff28e481f379f04c5fc1003ef63b
rozakassa22 <kassaroza27@gmail.com>
2024-06-17 09:59:10 +0300

    updates

--- final_project_web/store/info/info_Api.ts
+++ final_project_web/store/info/info_Api.ts
@@ -1,50 +1,47 @@
-const jwt = require("jsonwebtoken");
-import type { NextApiRequest, NextApiResponse } from 'next'
-import {URL} from '../host';
-
-
+// store/info/info_Api.ts
 export async function fetchInfoQuestion() {
-   
     try {
-        const response = await fetch(`${URL}/information/getNumberOfAllQuestion`, {
-            method: 'GET',
-            headers: {
-                Authorization: '',
-                'Content-Type': 'application/json',
-            },
-        });
+      const response = await fetch(`${URL}/information/getNumberOfAllQuestion`, {
+        method: 'GET',
+        headers: {
+          Authorization: '',
+          'Content-Type': 'application/json',
+        },
+      });
   
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.message);
-        }
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
   
-       
+      const data = await response.json();
+      return data; // Ensure this returns an object with a `count` property
     } catch (error) {
-        console.error('Error fetching info:', error);
-        throw error;
+      console.error('Error fetching info:', error);
+      throw error;
     }
   }
+  
   export async function fetchInfoUsers() {
-   
     try {
-        const response = await fetch(`${URL}/information/numberAllUser`, {
-            method: 'GET',
-            headers: {
-                Authorization: '',
-                'Content-Type': 'application/json',
-            },
-        });
+      const response = await fetch(`${URL}/information/numberAllUser`, {
+        method: 'GET',
+        headers: {
+          Authorization: '',
+          'Content-Type': 'application/json',
+        },
+      });
   
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.message);
-        }
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
   
-       
+      const data = await response.json();
+      return data; // Ensure this returns an object with a `count` property
     } catch (error) {
-        console.error('Error fetching info:', error);
-        throw error;
+      console.error('Error fetching info:', error);
+      throw error;
     }
   }
   
